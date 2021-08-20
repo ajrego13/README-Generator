66f1fff4b03e9be295bdef7344e5bb63304a0a43
ajrego13 <ajrego13@gmail.com>
2021-08-19 23:20:02 -0400

    wrote the list of questions for array of objects

--- Develop/index.js
+++ Develop/index.js
@@ -3,11 +3,56 @@ const inquiror = require('inquirer');
 const fs = require('fs'); 
 const generateMarkdown = require('./utils/generateMarkdown'); 
 // TODO: Create an array of questions for user input
+
+const licenses [
+
+]
 const questions = [
     {
-        input: "", 
-        
-    }
+        type: "input",
+        name: "title", 
+        message: "Enter the title of your project",
+    },
+    {
+        type: "input",
+        name: "email", 
+        message: "enter your email address",
+    },
+    {
+        type: "input",
+        name: "github", 
+        message: "Enter yourt Github username",
+    },
+    {
+        type: "input",
+        name: "description", 
+        message: "Enter a breif description of your project",
+    },
+    {
+        type: "input",
+        name: "installation", 
+        message: "Enter the steps required to install your project"",
+    },
+    {
+        type: "input",
+        name: "useage", 
+        message: "Provide instructions and an examples for use",
+    },
+    {
+        type: "input",
+        name: "contibuting", 
+        message: "list your collaborators",
+    },
+    {
+        type: "list",
+        name: "license", 
+        message: licenses,
+    },
+    {
+        type: "input",
+        name: "tests", 
+        message: "Exmplain how to run the tests included in your application",
+    },  
 ];
 
 // TODO: Create a function to write README file
