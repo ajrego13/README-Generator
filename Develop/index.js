092ab8f740d6b7daf1852d002bbef96abcaa5637
ajrego13 <ajrego13@gmail.com>
2021-08-19 23:06:08 -0400

    included packages needed for the app

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,14 @@
 // TODO: Include packages needed for this application
-
+const inquiror = require('inquirer'); 
+const fs = require('fs'); 
+const generateMarkdown = require('./utils/generateMarkdown'); 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        input: "", 
+        
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
